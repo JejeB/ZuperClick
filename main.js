0bb8fae8d0d128c95cbdcef79d08dcd922511d12
Jeremy Bonnefoi <jeremybonnefoi@yncrea.isen.fr>
2023-01-16 11:02:48 +0100

    add client board

--- main.js
+++ main.js
@@ -23,5 +23,45 @@ const firebaseConfig = {
 const fire_app = initializeApp(firebaseConfig);
 const db = getFirestore(fire_app);
 
-let pixi_app = new PIXI.Application({ width: 480, height: 854 });
+//Pixi APP
+const size = 600;
+
+const nb = 50;
+const tile_size = size/nb;
+let pixi_app = new PIXI.Application({ width: size, height: size });
 document.body.appendChild(pixi_app.view);
+
+
+for(let y=0;y<nb;y++){
+  for(let x=0;x<nb;x++){
+    createTile(x,y);
+  }
+}
+
+function createTile(x,y){
+  const tile = new PIXI.Graphics();
+
+  tile.interactive =true;
+  tile.on('pointerover',onPointerHover,tile)
+  tile.on('pointerout',onPointerOut,tile)
+  tile.on('pointertap',()=>{onPointerTap(x,y)},tile)
+  //Draw tile
+  tile.beginFill(0xFFFFFF);
+  tile.drawRect(x*tile_size,y*tile_size,tile_size,tile_size);
+  tile.endFill();
+  pixi_app.stage.addChild(tile);
+}
+
+
+
+function onPointerHover() {
+  this.tint = 0x666666;
+}
+
+function onPointerOut() {
+  this.tint = 0xFFFFFF;
+}
+
+function onPointerTap(x,y) {
+  console.log(x + " "+y);
+}
